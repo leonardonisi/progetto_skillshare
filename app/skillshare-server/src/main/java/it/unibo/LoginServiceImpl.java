37acb778a33520a7f23e19b326c5398d7a51b02f
Leo <leonardo.nisi5@gmail.com>
2026-06-11 21:25:50 +0200

    refactor con aggiunta database

--- app/skillshare-server/src/main/java/it/unibo/LoginServiceImpl.java
+++ app/skillshare-server/src/main/java/it/unibo/LoginServiceImpl.java
@@ -1,6 +1,9 @@
 package it.unibo;
 
 import com.google.gwt.user.server.rpc.jakarta.RemoteServiceServlet;
+import org.mapdb.DB;
+import org.mapdb.Serializer;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * The server side implementation of the RPC service.
@@ -8,6 +11,12 @@ import com.google.gwt.user.server.rpc.jakarta.RemoteServiceServlet;
 @SuppressWarnings("serial")
 public class LoginServiceImpl extends RemoteServiceServlet { // implements
     // LoginService {
+
+    private static final DB db = DatabaseCore.getDB();
+    private static final ConcurrentMap<String, String> dbUtenti = db
+            .hashMap("utenti", Serializer.STRING, Serializer.STRING)
+            .createOrOpen();
+
     public String authenticate(String username, String password) {
         // Verify that the input is valid.
         if (!FieldVerifier.isValidName(username)) {
@@ -16,15 +25,14 @@ public class LoginServiceImpl extends RemoteServiceServlet { // implements
             throw new IllegalArgumentException(
                     "Name must be at least 4 characters long");
         }
-        if (!"admin".equals(username)) {
+        if (!dbUtenti.containsKey(username)) {
             throw new IllegalArgumentException("Username inesistente");
         }
-        if ("admin".equals(username) && "password123".equals(password)) {
-            return username;
-        }
-        if ("admin".equals(username) && !"password123".equals(password)) {
+        String passwordCorretta = dbUtenti.get(username);
+
+        if (!passwordCorretta.equals(password)) {
             throw new IllegalArgumentException("Password errata");
         }
-        return null;
+        return username;
     }
 }
