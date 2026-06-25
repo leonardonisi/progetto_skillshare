ad26cba8d00ad1e90c5fcbcef3ca032f2af86614
Leo <leonardo.nisi5@gmail.com>
2026-06-25 09:44:29 +0200

    aggiunta del pattern builder per annuncio

--- app/skillshare-shared/src/main/java/it/unibo/Annuncio.java
+++ app/skillshare-shared/src/main/java/it/unibo/Annuncio.java
@@ -3,7 +3,6 @@ package it.unibo;
 import java.io.Serializable;
 
 public class Annuncio implements Serializable {
-    private static final long serialVersionUID = 1L;
 
     private String titolo;
     private String categoria;
@@ -15,14 +14,13 @@ public class Annuncio implements Serializable {
     public Annuncio() {
     }
 
-    public Annuncio(String titolo, String categoria, String skillOfferta, String controprestazioneCercata,
-            String disponibilita, String utenteId) {
-        this.titolo = titolo;
-        this.categoria = categoria;
-        this.skillOfferta = skillOfferta;
-        this.controprestazioneCercata = controprestazioneCercata;
-        this.disponibilita = disponibilita;
-        this.utenteId = utenteId;
+    private Annuncio(Builder builder) {
+        this.titolo = builder.titolo;
+        this.categoria = builder.categoria;
+        this.skillOfferta = builder.skillOfferta;
+        this.controprestazioneCercata = builder.controprestazioneCercata;
+        this.disponibilita = builder.disponibilita;
+        this.utenteId = builder.utenteId;
     }
 
     // Getter e Setter
@@ -30,23 +28,89 @@ public class Annuncio implements Serializable {
         return titolo;
     }
 
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
     public String getCategoria() {
         return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
     }
 
     public String getSkillOfferta() {
         return skillOfferta;
     }
 
-    public String getControprestazioneCercata() {
+    public void setOffro(String skillOfferta) {
+        this.skillOfferta = skillOfferta;
+    }
+
+    public String getControprestazione() {
         return controprestazioneCercata;
+    }
+
+    public void setControprestazione(String controprestazioneCercata) {
+        this.controprestazioneCercata = controprestazioneCercata;
     }
 
     public String getDisponibilita() {
         return disponibilita;
     }
 
-    public String getUtenteId() {
+    public void setDisponibilita(String disponibilita) {
+        this.disponibilita = disponibilita;
+    }
+
+    public String getUtente() {
         return utenteId;
+    }
+
+    public void setUtente(String utenteId) {
+        this.utenteId = utenteId;
+    }
+
+    // Builder
+    public static class Builder {
+        private String titolo;
+        private String categoria;
+        private String skillOfferta;
+        private String controprestazioneCercata;
+        private String disponibilita;
+        private String utenteId;
+
+        public Builder() {
+        }
+
+        public Builder titolo(String titolo) {
+            this.titolo = titolo;
+            return this;
+        }
+
+        public Builder categoria(String categoria) {
+            this.categoria = categoria;
+            return this;
+        }
+
+        public Builder skillOfferta(String skillOfferta) {
+            this.skillOfferta = skillOfferta;
+            return this;
+        }
+
+        public Builder controprestazioneCercata(String controprestazioneCercata) {
+            this.controprestazioneCercata = controprestazioneCercata;
+            return this;
+        }
+
+        public Builder disponibilita(String disponibilita) {
+            this.disponibilita = disponibilita;
+            return this;
+        }
+
+        public Annuncio build() {
+            return new Annuncio(this);
+        }
     }
 }
