9eb80a80d22be05c5876277cd1ed69e4953c3fb3
Pietro Brunelli <pietro.brunelli2@studio.unibo.it>
2026-07-02 19:42:53 +0200

    Definizione interfacce RPC per la dashboard Mie Richieste

---
+++ app/skillshare-shared/src/main/java/it/unibo/RichiesteService.java
@@ -0,0 +1,11 @@
+package it.unibo;
+
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("richieste")
+public interface RichiesteService extends RemoteService {
+    List<Annuncio> getMieRichieste(String username);
+}
